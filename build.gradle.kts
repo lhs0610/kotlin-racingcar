892232abe3a306fd40b9512b7c2e599e3ae588df
cju1016 <cju1016@midasin.com>
2023-07-10 15:10:39 +0900

    Chore : Gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
-    testImplementation("org.assertj", "assertj-core", "3.22.0")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
+    testImplementation("io.kotest:kotest-property-jvm:5.5.5")
 }
 
 tasks {
