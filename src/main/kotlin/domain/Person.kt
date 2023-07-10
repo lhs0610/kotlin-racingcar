39cc77167d29414f479d8a5b930967dda0491ec8
SH <30501257+Jinni01@users.noreply.github.com>
2023-07-10 17:15:36 +0900

    1 단계 - 코틀린 기초 (#4)
    * feat: java to kotlin
    
    * feat: Person class kotest 이용해 테스트 케이스 작성
    
    ---------
    
    Co-authored-by: suj0324 <suj0324@midasit.com>

---
+++ src/main/kotlin/domain/Person.kt
@@ -0,0 +1,8 @@
+data class Person(val name: String, val age: Int, var nickname: String?) {
+
+    companion object {
+        fun of(name: String, age: Int): Person {
+            return Person(name, age, null)
+        }
+    }
+}
