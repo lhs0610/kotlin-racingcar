659d0299fed8940ad9378d1cc97e36f35d5a4c15
한 용구 <hyyg111@naver.com>
2023-07-12 22:42:52 +0900

    feat : 자동차 경주 최종 우승자 출력

--- src/main/kotlin/racingcar/ui/ResultView.kt
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -14,8 +14,11 @@ fun printRaceResult(raceResult: RaceResultDto) {
     }
 
     println(result)
+    println()
+    println("${raceResult.winners.joinToString(WINNER_SEPARATOR)}가 최종 우승했습니다.")
 }
 
 private const val MOVED_DISTANCE = "-"
 private const val RACING_CAR_SEPARATOR = "\n"
 private const val STEP_SEPARATOR = "\n\n"
+private const val WINNER_SEPARATOR = ","
