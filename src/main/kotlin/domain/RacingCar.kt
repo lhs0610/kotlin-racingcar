e7b5a21ea736caf19f5f48d675c62365ba6c483c
Jinni01 <dml21@naver.com>
2023-07-17 21:24:37 +0900

    feat: racingCar attemptMove 메서드 추가

--- src/main/kotlin/domain/RacingCar.kt
+++ src/main/kotlin/domain/RacingCar.kt
@@ -6,6 +6,15 @@ class RacingCar() {
     val position: Int
         get() = _position.position
 
+    fun attemptMove(distance: Int) {
+        val randomInt = (0..10).random()
+        if (randomInt < 4) {
+            return
+        }
+
+        move(distance)
+    }
+
     fun move(distance: Int) {
         _position.move(distance)
     }
