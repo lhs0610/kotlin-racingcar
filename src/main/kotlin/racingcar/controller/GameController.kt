a4cf281056bb8614c01c7fcaaf26c429231ecf79
suj0324 <suj0324@midasit.com>
2023-07-18 20:30:38 +0900

    feat: GameController / GameService 싱글턴 클래스 추가

---
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -0,0 +1,11 @@
+package racingcar.controller
+
+import racingcar.service.GameService
+import racingcar.strategy.RandomStrategy
+
+object GameController {
+
+    fun startRacingGame() {
+
+    }
+}
---
+++ src/main/kotlin/racingcar/service/GameService.kt
@@ -0,0 +1,13 @@
+package racingcar.service
+
+import racingcar.component.Race
+import racingcar.dto.RaceHistoryDto
+import racingcar.strategy.RaceStrategy
+
+object GameService {
+
+    fun startRacingGame(racingcarQuantity: Int, totalStep: Int, raceStrategy: RaceStrategy): RaceHistoryDto {
+        val racingGame = Race(racingcarQuantity, totalStep, raceStrategy)
+        return racingGame.start()
+    }
+}
