f7d07a4d384dbf39ee56088e22598beb99a3c25b
kks1023 <kks1023@midasin.com>
2023-07-24 20:10:09 +0900

    feat: 사칙연산 문자열 결과값 도출

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -2,10 +2,34 @@ import java.util.*
 
 class Calculator(val strValue: String) {
 
-    val strQueue: Queue<String> = LinkedList()
+    val strList = LinkedList<String>()
+    private var result = Number(0.0)
 
     init {
-        val splitStr = this.strValue.split(" ")
-        this.strQueue.addAll(splitStr)
+        if (strValue.isBlank()) {
+            throw IllegalArgumentException()
+        }
+        strList.addAll(this.strValue.split(" "))
+    }
+
+    fun run(): Number {
+        result = Number(strList[0])
+        for (idx in 1 until strList.size step 2) {
+            val op = strList[idx]
+            val n2 = Number(strList[idx + 1])
+
+            result = calculate(result, n2, op)
+        }
+        return result
+    }
+
+    private fun calculate(n1: Number, n2: Number, op: String): Number {
+        return when (op) {
+            "+" -> n1 + n2
+            "-" -> n1 - n2
+            "*" -> n1 * n2
+            "/" -> n1 / n2
+            else -> throw IllegalArgumentException()
+        }
     }
 }
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -17,14 +17,14 @@ class CalculatorTest : FunSpec({
 
     test("사칙연산 문자열을 분리한다.") {
         val actual = Calculator(successStrValue)
-        actual.strQueue.size shouldBe 7
-        actual.strQueue.poll() shouldBe "2"
-        actual.strQueue.poll() shouldBe "+"
-        actual.strQueue.poll() shouldBe "3"
-        actual.strQueue.poll() shouldBe "*"
-        actual.strQueue.poll() shouldBe "4"
-        actual.strQueue.poll() shouldBe "/"
-        actual.strQueue.poll() shouldBe "2"
+        actual.strList.size shouldBe 7
+        actual.strList.poll() shouldBe "2"
+        actual.strList.poll() shouldBe "+"
+        actual.strList.poll() shouldBe "3"
+        actual.strList.poll() shouldBe "*"
+        actual.strList.poll() shouldBe "4"
+        actual.strList.poll() shouldBe "/"
+        actual.strList.poll() shouldBe "2"
     }
 
     test("숫자 객체를 구현한다.") {
@@ -42,15 +42,16 @@ class CalculatorTest : FunSpec({
         (n1 / n2).value shouldBe 1.5
     }
 
-    test("슷자가 아닌 문자의 입력을 구별한다.") {
-
+    test("사칙연산 문자열에 대한 올바른 결과값을 출력한다.") {
+        val actual = Calculator(successStrValue).run()
+        actual.value shouldBe 10
     }
 
     test("사칙연산 순서에 올바르지 않는 문자열을 구별한다.") {
 
     }
 
-    test("사칙연산 문자열에 대한 올바른 결과값을 출력한다.") {
+    test("올바르지 않은 문자의 입력을 구별한다.") {
 
     }
 })
