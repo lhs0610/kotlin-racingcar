c436db1f263e73bc4c9a13e1a4b065fe28d7d94b
suj0324 <suj0324@midasit.com>
2023-07-26 17:31:59 +0900

    refactor: behavior spec 변경 작업 진행
    - RacingCarTest

--- src/test/kotlin/racingcar/RacingCarTest.kt
+++ src/test/kotlin/racingcar/RacingCarTest.kt
@@ -1,31 +1,33 @@
 package racingcar
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.core.spec.style.ExpectSpec
 import io.kotest.matchers.shouldBe
+import racingcar.constant.Message
 import racingcar.domain.RacingCar
 
-class RacingCarTest : ExpectSpec({
+class RacingCarTest : BehaviorSpec({
 
-    context("RacingCar test") {
-        expect("construct test") {
-            val racingCar = RacingCar.of("Jinni", 0)
-
-            racingCar.getDistance() shouldBe 0
-            racingCar.getRacerName() shouldBe "Jinni"
-        }
-
-        expect("construct test - name length error") {
-            shouldThrow<IllegalArgumentException> {
-                RacingCar.of("JinniJinni", 0)
+    Given("racingCar") {
+        val racingCar = RacingCar.of("Jinni", 0)
+        When("레이서 이름과 거리를 생성자 인자로 RacingCar를 생성하면") {
+            Then("프로퍼티에 동일한 값이 설정된 RacingCar를 반환한다") {
+                racingCar.getDistance() shouldBe 0
+                racingCar.getRacerName() shouldBe "Jinni"
             }
         }
-
-        expect("move test") {
-            val racingCar = RacingCar.of("Jinni", 0)
+        When("이동 거리가 0 인 상태에서 attemptMove 메서드를 호출하면(무조건 이동 정책 사용)") {
             racingCar.attemptMove { true }
-
-            racingCar.getDistance() shouldBe 1
+            Then("racingCar의 이동 거리는 1이 되어야 한다") {
+                racingCar.getDistance() shouldBe 1
+            }
+        }
+        When("레이서 이름이 글자 수 제한을 초과하면") {
+            val exception = shouldThrow<IllegalArgumentException> { RacingCar.of("JinniJinni", 0) }
+            Then("에러가 발생한다") {
+                exception.message shouldBe Message.INVALID_INPUT_VALUE_CANNOT_HIGHER_THEN_FIVE.message
+            }
         }
     }
 })
